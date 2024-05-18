e585f47a10a5b9635161dd7a104be36fae2662f3
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-05-18 18:45:41 +0600

    use tanstack query for fetching rooms data

--- client/src/components/Shared/LoadingSpinner.jsx
+++ client/src/components/Shared/LoadingSpinner.jsx
@@ -1,22 +1,20 @@
-import PropTypes from 'prop-types'
-import { ScaleLoader } from 'react-spinners'
+import PropTypes from "prop-types";
+import { ScaleLoader } from "react-spinners";
 
 const LoadingSpinner = ({ smallHeight }) => {
   return (
     <div
-      className={` ${smallHeight ? 'h-[250px]' : 'h-[70vh]'}
-      flex 
-      flex-col 
-      justify-center 
-      items-center `}
+      className={`${
+        smallHeight ? "h-[250px]" : "h-[70vh]"
+      } flex flex-col justify-center items-center`}
     >
-      <ScaleLoader size={100} color='red' />
+      <ScaleLoader size={100} color="red" />
     </div>
-  )
-}
+  );
+};
 
 LoadingSpinner.propTypes = {
   smallHeight: PropTypes.bool,
-}
+};
 
-export default LoadingSpinner
+export default LoadingSpinner;
